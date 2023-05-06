bbe7086eef2a34eb432245cf585dd744537f26f3
unluckyProgDestroyer <viktorgyenes35@gmail.com>
2023-05-06 12:06:45 +0200

    Resource server project init

---
+++ backend/resource_server/src/main/java/hu/sigmalimited/sigmawebshop/SigmawebshopApplication.java
@@ -0,0 +1,13 @@
+package hu.sigmalimited.sigmawebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmawebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmawebshopApplication.class, args);
+	}
+
+}
