bb700a4b8e415326a5484fcd7ce3fa1df42ba63c
unluckyProgDestroyer <viktorgyenes35@gmail.com>
2023-05-07 16:05:44 +0200

    setup file storage feature

--- backend/resource_server/src/main/java/hu/sigmalimited/sigmawebshop/SigmawebshopApplication.java
+++ backend/resource_server/src/main/java/hu/sigmalimited/sigmawebshop/SigmawebshopApplication.java
@@ -1,14 +1,24 @@
 package hu.sigmalimited.sigmawebshop;
 
+import hu.sigmalimited.sigmawebshop.feature.file.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import redis.clients.jedis.Jedis;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SigmawebshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigmawebshopApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			//storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
